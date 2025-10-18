cabc0fcd669857a8a450498042f1be5a2c389d75
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-10-18 23:27:38 +0530

    initial commit

---
+++ src/main/java/com/educonnect/EduConnectPortalApplication.java
@@ -0,0 +1,13 @@
+package com.educonnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduConnectPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduConnectPortalApplication.class, args);
+	}
+
+}
