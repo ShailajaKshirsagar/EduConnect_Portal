b3c9bdf3723dd330353c44b5ae5676f4e11c4048
Shailaja kshirsagar <shailajakshirsagarr@gmail.com>
2025-10-22 15:30:43 +0530

    Search Product api implemented (#6)

--- src/main/java/com/educonnect/service/ProductService.java
+++ src/main/java/com/educonnect/service/ProductService.java
@@ -2,6 +2,10 @@ package com.educonnect.service;
 
 import com.educonnect.entity.Product;
 
+import java.util.List;
+
 public interface ProductService {
     String saveProduct(Product product);
+
+    List<Product> getProductByKey(String keyword);
 }
