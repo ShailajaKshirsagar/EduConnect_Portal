e38b81dbe91444c7d8ae90b2a37c89a13e754671
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-10-27 22:53:36 +0530

    Get post comments by postid api implemented

--- src/main/java/com/educonnect/controller/BlogCommentController.java
+++ src/main/java/com/educonnect/controller/BlogCommentController.java
@@ -4,6 +4,8 @@ import com.educonnect.dto.CommentRequestDto;
 import com.educonnect.dto.CommentResponseDto;
 import com.educonnect.service.BlogCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -21,7 +23,13 @@ public class BlogCommentController {
 
         String username = authentication.getName();
         CommentResponseDto response = blogCommentService.createComment(postid,requestDto,username);
-
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{postid}/getCommentsByPostId")
+    public Page<CommentResponseDto> getComments(@PathVariable long postid,@RequestParam(defaultValue = "0") int page ,
+                                                @RequestParam(defaultValue = "2") int size){
+
+        return blogCommentService.getCommentsByPostId(postid, PageRequest.of(page,size));
     }
 }
--- src/main/java/com/educonnect/repository/BlogCommentRepo.java
+++ src/main/java/com/educonnect/repository/BlogCommentRepo.java
@@ -1,9 +1,16 @@
 package com.educonnect.repository;
 
 import com.educonnect.entity.BlogComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogCommentRepo extends JpaRepository<BlogComment,Long> {
+
+    @Query("SELECT c FROM BlogComment c WHERE c.post.post_id =:post_id")
+    Page<BlogComment> findByPostId(@Param("post_id") long post_id, Pageable pageable);
 }
